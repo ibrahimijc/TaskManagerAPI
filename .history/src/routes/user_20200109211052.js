c9e714b830286809197cc708133573fc02141b9b
ibrahimijc <ibrahimjawedijc@gmail.com>
2020-01-09 23:13:02 +0500

    formatting issues and working on generatingAuthTokens

---
+++ .history/src/routes/user_20200109211052.js
@@ -0,0 +1,102 @@
+
+const express = require('express');
+const router = new express.Router();
+const User = require('../models/User')
+
+router.post('/users', async (req,res)=>{
+    
+    try{
+      const user = new User(req.body);
+      await user.save(); // this will break if not successfull
+      res.status(201).send(user);
+    }catch(e){
+        res.status(400).send(e.message);
+
+    }
+
+   
+})
+
+
+router.post('/users/login', async (req,res) =>{
+    try{
+        console.log(req.body.password);
+        const user = User.findByCredentials(req.body.email,req.body.Password);
+    }catch(e){
+
+    }
+})
+
+
+
+
+router.get('/users',async (req,res)=>{
+   try{ 
+    const users = await User.find({});
+    res.send(users)
+    }
+    catch(e){
+     res.status(500).send();
+     console.log(e.message);
+    }
+})
+
+
+router.get('/user/:id',async (req,res) =>{
+    try{
+      const user = await  User.findById(req.params.id);
+      res.status(200).send(user);
+    }
+    catch(e){
+      res.status(404).send();
+    }
+})
+
+
+router.patch('/user/:id', async(req,res)=>{
+    
+    const allowedUpdates = ["UserName","Email","Password","age"];
+    const updates = Object.keys(req.body);
+
+    let isValidOperation = updates.every((update)=>{
+        return allowedUpdates.includes(update);
+    })
+    if (!isValidOperation){
+        res.status(400).send({'error':'invalid update'});
+    }
+    try{
+
+        const user = await User.findById(req.params.id);
+
+        updates.forEach((update) => {
+            user[update] = req.body[update];
+        })
+        await user.save();
+        //const user = await User.findByIdAndUpdate(req.params.id,req.body,{new: true, runValidators: true});
+        if (!user){
+            return res.status(404).send();
+        }
+        
+
+        res.status(200).send(user);
+    }catch(e){
+        res.status(404).send();
+    }
+});
+
+router.delete('/user/:id', async(req,res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if (!user){
+            return res.status(400).send();
+        }
+
+        res.send(user);
+    }catch(e){
+        res.status(500).send();
+    }
+})
+
+
+module.exports = router;
