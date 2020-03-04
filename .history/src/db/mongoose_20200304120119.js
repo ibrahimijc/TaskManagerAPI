f33dbdb08d0b87e93c46360ce8ba698304709d06
ibrahimijc <ibrahimjawedijc@gmail.com>
2020-03-04 12:05:01 +0500

    'test'

--- .history/src/db/mongoose_20200304120119.js
+++ .history/src/db/mongoose_20200304120119.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 mongoose.set('useCreateIndex', true);
 
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 dotenv.config();
 
 
